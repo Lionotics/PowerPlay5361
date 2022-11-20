20b53ec1f61619c9d0b3866168c2bee5188988e3
computerjoe314 <72146037+prosenfeld@users.noreply.github.com>
2022-11-20 11:33:42 -0500

    Auto path work ft. Monsey Trails
    Time to go actually make it do stuff woohoo

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
@@ -1,5 +1,7 @@
 package com.example.meepmeep;
 
+import static java.lang.Math.toRadians;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
 import com.acmerobotics.roadrunner.geometry.Vector2d;
@@ -12,98 +14,41 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-//        MeepMeep meepMeep = new MeepMeep(800);
-//
-//        AddTrajectorySequenceCallback redCarouselSide =
-//                drive -> drive.trajectorySequenceBuilder(new Pose2d(-30.5, -62, Math.toRadians(270)))
-//                        .setReversed(true)
-//                        .splineTo(new Vector2d(-32, -32), Math.toRadians(15)) // start extending before this
-//                        .waitSeconds(3.0) // actually deposit, retract
-//                        .turn(Math.toRadians(165))
-//                        .splineTo(new Vector2d(-60, -40), Math.toRadians(270))
-//                        .waitSeconds(3.0) // do ducks
-//                        .forward(5.0)
-//                        .build();
-//
-//        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-//                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-//                .setConstraints(100, 200, Math.toRadians(180), Math.toRadians(180), 14)
-//                .followTrajectorySequence(drive ->
-//                        drive.trajectorySequenceBuilder(new Pose2d(-38, -60, Math.toRadians(90)))
-//                                .splineTo(new Vector2d(-13, -60), Math.toRadians(90))
-//                                .splineTo(new Vector2d(-13, -35), Math.toRadians(45))
-//                                .build()
-//
-//                )
-//
-//
-//
-//
-//
-//        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_KAI_DARK)
-//                .setDarkMode(true)
-//                .setBackgroundAlpha(0.95f)
-//                .addEntity(myBot)
-//                .start();
+        MeepMeep meepMeep = new MeepMeep(600);
 
-        AddTrajectorySequenceCallback redCarouselSide =
-                drive -> drive.trajectorySequenceBuilder(new Pose2d(-30.5, -62, Math.toRadians(270)))
-                        .setReversed(true)
-                        .splineTo(new Vector2d(-32, -32), Math.toRadians(15)) // start extending before this
-                        .waitSeconds(3.0) // actually deposit, retract
-                        .turn(Math.toRadians(165))
-                        .splineTo(new Vector2d(-60, -40), Math.toRadians(270))
-                        .waitSeconds(3.0) // do ducks
-                        .forward(5.0)
-                        .build();
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(45,30,toRadians(180),toRadians(180),15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(38.8, -61.5, toRadians(90)))
+                                .strafeTo(new Vector2d(38.8-25,-61.5+5))
+                                .lineToLinearHeading(new Pose2d(8.8,-30.5,toRadians(90+45)))
+                                .waitSeconds(2)
+                                .forward(-5)
+                                .lineToLinearHeading(new Pose2d(11,-10,toRadians(0)))
+                                .lineToLinearHeading(new Pose2d(62,-12,toRadians(0)))
+                                .waitSeconds(2)
+                                .forward(-20)
+                                .lineToLinearHeading(new Pose2d(29, -5, toRadians(135)))
+                                .waitSeconds(2)
+                                // 1
+//                                .lineToLinearHeading(new Pose2d(55,-10,toRadians(90)))
+                                // 2
+//                                .lineToLinearHeading(new Pose2d(35,-10,toRadians(90)))
+                                // 3
+                                .forward(-10)
+                                .lineToLinearHeading(new Pose2d(60,-10,toRadians(90)))
 
-        AddTrajectorySequenceCallback blueCarouselSide =
-                drive -> drive.trajectorySequenceBuilder(new Pose2d(-30.5, 62, Math.toRadians(-270)))
-                        .setReversed(true)
-                        .splineTo(new Vector2d(-32, 32), Math.toRadians(-15)) // start extending before this
-                        .waitSeconds(3.0) // actually deposit, retract
-                        .turn(Math.toRadians(165))
-                        .splineTo(new Vector2d(-60, 40), Math.toRadians(-270))
-                        .waitSeconds(3.0) // do ducks
-                        .forward(5.0)
-                        .build();
 
-        AddTrajectorySequenceCallback redWarehouseSide =
-                drive -> drive.trajectorySequenceBuilder(new Pose2d(6.5, -62, Math.toRadians(270)))
-                        .setReversed(true)
-                        .splineTo(new Vector2d(8, -32), Math.toRadians(165)) // start extending before this
-                        .waitSeconds(3.0) // actually deposit, retract
-                        .turn(Math.toRadians(-75))
-                        .setReversed(false)
-                        .splineTo(new Vector2d(8, -60), Math.toRadians(270))
-                        .turn(Math.toRadians(90))
-                        .strafeTo(new Vector2d(8, -70))
-                        .forward(36.0)
-                        .build();
 
-        AddTrajectorySequenceCallback blueWarehouseSide  =
-                drive -> drive.trajectorySequenceBuilder(new Pose2d(6.5, 62, Math.toRadians(-270)))
-                        .setReversed(true)
-                        .splineTo(new Vector2d(8, 32), Math.toRadians(-165)) // start extending before this
-                        .waitSeconds(3.0) // actually deposit, retract
-                        .turn(Math.toRadians(-75))
-                        .setReversed(false)
-                        .splineTo(new Vector2d(8, 60), Math.toRadians(-270))
-                        .turn(Math.toRadians(-90))
-                        .strafeTo(new Vector2d(8, 70))
-                        .forward(36.0)
-                        .build();
+                                .build()
+                );
 
-//        MeepMeep mm = new MeepMeep(800)
-//                // Set field im800age
-//                .setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
-//                // Set theme
-//                .setTheme(new ColorSchemeRedDark())
-//                // Background opacity from 0-1
-//                .setBackgroundAlpha(1f)
-//                // Set constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-//                .setConstraints(45, 30, Math.toRadians(233), Math.toRadians(180), 14)
-//                .followTrajectorySequence(redCarouselSide)
-//                .start();
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_KAI_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
     }
 }
