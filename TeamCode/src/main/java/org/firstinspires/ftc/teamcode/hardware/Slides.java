9eb853ff476e4f204c4da7c8ee8d769bb7dee712
computerjoe314 <72146037+prosenfeld@users.noreply.github.com>
2023-01-24 15:52:47 -0500

    Motion Profiling. Maybe.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Slides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Slides.java
@@ -3,9 +3,14 @@ package org.firstinspires.ftc.teamcode.hardware;
 import com.ThermalEquilibrium.homeostasis.Controllers.Feedback.BasicPID;
 import com.ThermalEquilibrium.homeostasis.Parameters.PIDCoefficients;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.profile.MotionProfile;
+import com.acmerobotics.roadrunner.profile.MotionProfileGenerator;
+import com.acmerobotics.roadrunner.profile.MotionState;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 // Class representing our vertical slides
 
@@ -26,18 +31,27 @@ public class Slides extends Mechanism{
     public static double Kp = 0;
     public static double Ki = 0;
     public static double Kd = 0;
+    // These are bogus values!
+    public static double maxVel = 10;
+    public static double maxAccel = 10;
+    public static double maxJerk = 0;
 
+    private MotionProfile profile;
+    private DcMotorEx lift;
 
-    private DcMotor lift;
+    private ElapsedTime timer;
 
     @Override
     public void init(HardwareMap hwMap) {
-        lift = hwMap.dcMotor.get("arm");
+        lift = (DcMotorEx) hwMap.dcMotor.get("arm");
         lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         lift.setDirection(DcMotorSimple.Direction.REVERSE);
         lift.setTargetPosition(0);
         lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        this.timer = new ElapsedTime();
+        timer.reset();
     }
 
     public void hold(){
@@ -84,8 +98,21 @@ public class Slides extends Mechanism{
     }
 
     public void moveTowardsGoal(){
+
+
         BasicPID controller = new BasicPID(new PIDCoefficients(Kp, Ki, Kd));
         double power = controller.calculate(targetPosition, this.getPosition()) + Kg;
+        lift.setPower(power);
+    }
+    // Warning: Very untested and sketchy code below!
+    public void setupMP(){
+        timer.reset();
+        this.profile = MotionProfileGenerator.generateSimpleMotionProfile(new MotionState(this.getPosition(),0),new MotionState(targetPosition,0), maxVel,maxAccel);
+    }
+    public void moveTowardsGoalMP(){
+        MotionState state = this.profile.get(timer.seconds());
+        BasicPID controller = new BasicPID(new PIDCoefficients(Kp, Ki, Kd));
+        double power = controller.calculate(state.getX(), this.getPosition()) + Kg;
         lift.setPower(power);
     }
 
